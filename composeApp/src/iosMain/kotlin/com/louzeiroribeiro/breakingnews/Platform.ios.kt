9656f89c4be6fd6e84ee838be9cd23f3c538d64e
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-04-09 11:16:10 -0300

    Create About Screen

--- composeApp/src/iosMain/kotlin/com/louzeiroribeiro/breakingnews/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/louzeiroribeiro/breakingnews/Platform.ios.kt
@@ -2,8 +2,12 @@ package com.louzeiroribeiro.breakingnews
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
 
-actual fun getPlatform(): Platform = IOSPlatform()
+actual class Platform {
+    actual  val osName: String
+        get() = UIDevice.currentDevice.systemName
+    actual val osVersion: String
+        get() = UIDevice.currentDevice.systemVersion
+    actual val deviceModel: String
+        get() = UIDevice.currentDevice.model
+}
